5edb047a1a4b26020f6399b1469188382d37da78
ampleeve <e@ampleev.com>
2016-12-11 14:33:42 +0300

    comments final

--- lib/db.class.php
+++ lib/db.class.php
@@ -63,17 +63,53 @@ class db{
 
     }
 
+    /**
+     * @param $query
+     * @param array $params
+     * @return mixed
+     * До данного метода я добрался, т.к. разбирал метод селект, сюда передается запрос и некие параметры
+     * в виде массива. Смотрим что делает данный метод. В нашем случае он получит запрос
+     * SELECT id_category, name FROM categories WHERE status=:status AND parent_id =:parent_id
+     * и в качестве параметров: 'status' => Status::Active(берется значение из класса
+     * статус где эктив соответствует единице),
+     * 'parent_id' => $parentId
+     */
     public function Query($query, $params = array()){
+        // переменной рез присваивается значение метода препэйр и ему передается запрос. Похоже что ему присваивается даже
+        // не значение, а сам объект. Сейчас выведем его и посмотрим..Да, действительно это объект PDO. В нем, после выполнения
+        // $res = $this->db->prepare($query); только одно значение в свойстве ["queryString"]=>
+        //string(87) "SELECT id_category, name FROM categories WHERE status=:status AND parent_id =:parent_id"
+        // После использования метода экзекъют тоже никаких изменений. По сути возвращается строка запроса
 
         $res = $this->db->prepare($query);
         $res->execute($params);
+        //echo '<pre>';
+        //var_dump($res);
+        //echo '</pre>';die();
         return $res;
 
     }
 
+    /**
+     * @param $query
+     * @param array $params
+     * @return mixed
+     * Добрался я сюда т.к. в классе категорий вызывается данный метод для получения категорий. Видим что ему передается запрос
+     * и параметры какие-то и сразу показано что параметры - это массив. В нашем конкретном случае передается запрос
+     * "SELECT id_category, name FROM categories WHERE status=:status AND parent_id =:parent_id" что означает
+     * верни ид категории и имя для тех значений в таблице категориес, в которых одинаковые статусы и одинаковые перент ид(гипотеза).
+     *В качестве параметров передается массив ассоциативный, в котором 'status' => Status::Active(берется значение из класса
+     * статус где эктив соответствует единице),
+     * 'parent_id' => $parentId (передается значение в вызываемый этот метод метод).
+     * Далее мы вимдим что вызываетсяметод query данного класса, пошел его смотреть. В общем, здесь речь идет
+     * работе с PDO - стандартная библиотека для работы с бд. Функция fetchAll - возвращает уже подготовленные данные.
+     */
     public function Select($query, $params = array()){
 
         $result = $this->Query($query, $params);
+        //echo '<pre>';
+        //var_dump($result->fetchAll());
+        //echo '</pre>';die();
 
         if($result){
 
